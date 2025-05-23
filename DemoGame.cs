2a9599582e31065bddebc42e91acff06e71d3dc8
Thomas Just <124365904+DRgreenT@users.noreply.github.com>
2025-05-23 03:43:49 +0300

    level edit

--- DemoGame.cs
+++ DemoGame.cs
@@ -1,6 +1,7 @@
 ﻿using MyFirstGameEngine.Log;
 using MyFirstGameEngine.Models;
 using MyFirstGameEngine.EngineLogic;
+using System.Diagnostics;
 
 namespace MyFirstGameEngine
 {
@@ -8,23 +9,27 @@ namespace MyFirstGameEngine
     {
         Sprite2D? player;
 
+        const float maxFallSpeed = 5.0f;
+        const float gravity = 0.0981f;
+        const float jumpForce = 4.75f;
+
         string[,] Map = {
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
-        {"w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w" },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "g", ".", ".", ".", ".", "." },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "e" },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "g", ".", "g", "g" },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "g", "g", ".", ".", ".", "." },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", "g", ".", ".", ".", ".", ".", "." },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
+        {".", ".", ".", ".", ".", ".", ".", ".", "g", ".", ".", ".", ".", ".", ".", "." },
+        {".", ".", ".", ".", "g", "g", "g", ".", ".", ".", ".", ".", ".", ".", ".", "." },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
+        {".", ".", ".", ".", ".", ".", ".", "g", "g", "g", "g", "g", "g", "g", ".", "." },
+        {".", ".", ".", ".", "g", "g", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
+        {".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
+        {"g", "g", "g", "g", "g", "g", "g", "g", "g", "g", "g", "g", "g", "g", "g", "g" },
         };
 
         public DemoGame() : base(new Vector2(800, 600), "Demo Game") {}
@@ -34,65 +39,49 @@ namespace MyFirstGameEngine
             ConsoleLog.Info("Game resources loaded!");
 
             player = new Sprite2D(new Vector2(10, 10), new Vector2(25, 35), "Base pack\\Player\\p1_front", "Player");
+            LoadLevel();
+        }
+
+        void LoadLevel()
+        {
             for (int i = 0; i < Map.GetLength(0); i++)
             {
                 for (int j = 0; j < Map.GetLength(1); j++)
                 {
-                    if (Map[i, j] == "w")
+                    if (Map[i, j] == "g")
                     {
                         Sprite2D wallSprite = new
                             (
                             new Vector2(j * 50, i * 35),
                             new Vector2(50, 35),
                             "Base pack\\Tiles\\stoneWall",
-                            "Wall"
-
+                            "Ground"
                             );
+                    }
+                    if (Map[i,j] == "e")
+                    {
+                        Sprite2D exit = new (
+                            new Vector2((j * 50)+7, (i * 35)+15),
+                            new Vector2(20,20), 
+                            "Base pack\\Tiles\\signExit",
+                            "Exit");
                     }
                 }
             }
-
-        }
-        public override void OnDraw()
-        {
-
-        }
-        public override void OnStart()
-        {
-
         }
 
-        bool Collisions()
-        {
-
-                Vector2 windowSize = Window.GetWindowSize();
-
-                return player.IsColliding("Wall") ||
-                       player.Position.X + player.Size.X > windowSize.X - 5 ||
-                       player.Position.Y + player.Size.Y > windowSize.Y - 5 ||
-                       player.Position.X < 0 ||
-                       player.Position.Y < 0;
-            
-        }
+        public override void OnDraw(){ }
+        public override void OnStart(){}
+              
         public override void OnUpdate()
         {
-            if (player != null && Window != null)
-            {
-                Vector2 playerPosOld = player.Position;
-                Vector2 moveDelta = InputHandler();
-                player.Position = player.Position + moveDelta;
-                if (Collisions())
-                {
-                    player.Position = playerPosOld;
-                }                          }
+            Move(player!,jumpForce,gravity,maxFallSpeed);
         }
 
         public override void OnStop()
         {
             ConsoleLog.Info("Game stopped!");
         }
-
-
 
         public override void KeyDown(object sender, KeyEventArgs e)
         {
